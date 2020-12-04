580efd8389d14d297619fd3082d2d37d7d7a19bf
darren-stoll <darrenstoll11@gmail.com>
2020-12-04 11:53:37 -0700

    Set unified topology to false on mongoose connect

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'pug');
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: false})
 
 var trainerSchema = new mongoose.Schema({
   name: {
