fe5ff8c90c1a55af2812402c709c2a3933b6952b
darren-stoll <darrenstoll11@gmail.com>
2020-12-04 12:35:08 -0700

    Still not working not sure what else to do

--- server.js
+++ server.js
@@ -14,13 +14,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'pug');
 
 // mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.connect(`mongodb+srv://${process.env.MONGO_URL}`, 
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.sg4wk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
 {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
-  user: process.env.MONGO_USER,
-  pass: process.env.MONGO_PASS,
-  dbName: process.env.MONGO_DB
+  useUnifiedTopology: false
 })
 
 var trainerSchema = new mongoose.Schema({
